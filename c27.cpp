23f670a38305feb3e7b2b9f23e89efe27e0628dd
Rehan Ali maan <130174371+RehanAliMaan@users.noreply.github.com>
2023-11-11 20:56:43 +0500

    Add files via upload

---
+++ c27.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+int main ()
+{
+	int n;
+	cout<<"enter the number"<<endl;
+	cin>>n;
+	switch(n)
+	{
+	
+		case1:
+			cout<<"friday";
+			break;
+			
+		case2:
+			cout<<"saturday";
+			break;
+		case3:
+			cout<<"sunday";
+			break;
+		case4:
+			cout<<"monday";
+			break;
+		case5:
+			cout<<"tuesday";
+			break;
+		case6:
+			cout<<"wednesday";
+			break;
+		case7:
+			cout<<"thursday";
+			break;
+			default:
+				cout<<"invalid number";
+				
+			
+	}
+	
+}
